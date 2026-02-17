bdaafb85e4fa21d17060811724120c9d32845a78
cat-phish <134035929+cat-phish@users.noreply.github.com>
2026-02-17 10:40:56 -0800

    changed caldav cache location

--- early-init.el
+++ early-init.el
@@ -2,3 +2,6 @@
 ;; niri, may not be necessary on other systems
 (setq frame-inhibit-implied-resize t)
 (setq frame-resize-pixelwise t)
+
+;; Force the frame to be maximized on startup to fill the Niri window
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
